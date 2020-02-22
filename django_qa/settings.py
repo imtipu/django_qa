01b6a19a68a125722125e7ec3198da0faede8066
Borhan Tipu <me@imtipu.me>
2020-02-22 20:42:53 +0600

    host updated

--- django_qa/settings.py
+++ django_qa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("HOST", default=["*"])
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
