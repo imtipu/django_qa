42b7d298799591b64d4cc5bcf92d96cd0d8521a7
Borhan Tipu <me@imtipu.me>
2020-02-22 20:38:31 +0600

    .env path added

--- django_qa/settings.py
+++ django_qa/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import environ
+# from dotenv import load_dotenv
 
 env = environ.Env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
