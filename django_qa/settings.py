e20215fb08f0eb72c448ed09d3ee935aebc10078
Borhan Tipu <me@imtipu.me>
2020-02-22 20:46:43 +0600

    host updated

--- django_qa/settings.py
+++ django_qa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['http://tipu00002.pythonanywhere.com/', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['tipu00002.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
